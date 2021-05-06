9d26c3cb7255b3fec345bcc6f855a91c83fc499c
MPThLee <mpthlee@gmail.com>
2021-05-07 00:39:56 +0900

    Fix file generate

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export class InternalDateTime {
     .set({ second: 0, millisecond: 0 });
 
   public toFileTimeFormat(): string {
-    return this.dt.toFormat('yyyy-LL-dd/HH_mm_ss');
+    return this.dt.toFormat('yyyy-LL/dd/HH_mm_ss');
   }
 
   public toInternalFormat(): string {
