5872e4c9445995d449b55bb66da5e2ca8bd2942e
MPThLee <mpthlee@gmail.com>
2021-05-08 02:57:00 +0900

    script changes.

--- .github/_initVolta.sh
+++ .github/_initVolta.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 IFS=';'
-ITEMS=node -p "Object.entries(require('./package.json').volta).map(i => i[0] + '@' + i[1]).join('$IFS')"
 
-for item in ITEMS; do
+# For run command below.
+volta install node --verbose
+ITEMS=`node -p "Object.entries(require('./package.json').volta).map(i => i[0] + '@' + i[1]).join('$IFS')"`
+
+for item in $ITEMS; do
     echo "Install $item via volta..."
-    volta install $item
+    volta install $item --verbose
 done
